0244034f1e49d34d5aba52ce78abbd53d35d703d
YashKinge <yashbkinge22@gmail.com>
2026-01-12 23:02:21 +0530

    Array Program

---
+++ workspace/src/com/java/array/CopyArray.java
@@ -0,0 +1,20 @@
+package com.java.array;
+
+public class CopyArray {
+
+	public static void main(String[] args) {
+
+		        int[] original = {10, 20, 30, 40, 50};
+		        int[] copy = new int[original.length];
+
+		        for (int i = 0; i < original.length; i++) {
+		            copy[i] = original[i];
+		        }
+		        System.out.println("Copied Array:");
+		        for (int i = 0; i < copy.length; i++) {
+		            System.out.print(copy[i] + " ");
+		        }
+		    
+	}
+
+}
