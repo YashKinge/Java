71f7c3c7402fe988a3e2ebe11e812a184bba7ace
YashKinge <yashbkinge22@gmail.com>
2026-01-12 23:00:12 +0530

    Array Program

---
+++ workspace/src/com/java/array/Reverse.java
@@ -0,0 +1,17 @@
+package com.java.array;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+		        int[] arr = {10, 20, 30, 40, 50};
+
+		        System.out.println("Array in reverse order:");
+        
+		        for (int i = arr.length - 1; i >= 0; i--) {
+		            System.out.print(arr[i] + " ");
+		        }
+
+	}
+
+}
