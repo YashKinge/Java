f48e4167f202af02cf3773b35fda370e10e84499
YashKinge <yashbkinge22@gmail.com>
2026-01-17 21:51:06 +0530

    added string program

---
+++ workspace/src/com/java/string/CompareStrings.java
@@ -0,0 +1,13 @@
+package com.java.string;
+
+public class CompareStrings {
+
+	public static void main(String[] args) {
+		        String a = "Hello";
+		        String b = "Hello";
+
+		        System.out.println(a.equals(b));
+
+	}
+
+}
