25506fbe6a0903eef609209379bc6c82baf0c4b8
YashKinge <yashbkinge22@gmail.com>
2026-01-17 21:49:17 +0530

    added string program

---
+++ workspace/src/com/java/string/CharAtExample.java
@@ -0,0 +1,11 @@
+package com.java.string;
+
+public class CharAtExample {
+
+	public static void main(String[] args) {
+		        String str = "Java";
+		        System.out.println(str.charAt(0));
+
+	}
+
+}
