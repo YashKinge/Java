d32d678069feba58e3f71f68ece25758c157ea1f
YashKinge <yashbkinge22@gmail.com>
2026-01-18 23:23:15 +0530

    added program

---
+++ workspace/src/com/java/string/UpperLowerCase.java
@@ -0,0 +1,13 @@
+package com.java.string;
+
+public class UpperLowerCase {
+
+	public static void main(String[] args) {
+        String str = "Java Programming";
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+	}
+
+}
