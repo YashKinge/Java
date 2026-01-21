61db46ec3e913c10031158c96a7533cd9cc7c8a5
YashKinge <yashbkinge22@gmail.com>
2026-01-21 22:12:52 +0530

    added program

---
+++ workspace/src/com/java/string/RemoveSpaces.java
@@ -0,0 +1,13 @@
+package com.java.string;
+
+public class RemoveSpaces {
+
+	public static void main(String[] args) {
+        String str = "Java is easy";
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+
+	}
+
+}
