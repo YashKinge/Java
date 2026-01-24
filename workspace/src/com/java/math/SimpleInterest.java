493b5cee288a0b1e6c3898b9d3239efcf25bdb32
YashKinge <yashbkinge22@gmail.com>
2026-01-24 21:59:21 +0530

    added program

---
+++ workspace/src/com/java/math/SimpleInterest.java
@@ -0,0 +1,12 @@
+package com.java.math;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+        int p = 1000, t = 2, r = 5;
+        int si = (p * t * r) / 100;
+        System.out.println("Simple Interest = " + si);
+
+	}
+
+}
