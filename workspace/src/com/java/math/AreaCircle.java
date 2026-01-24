43f3259639c1069a934efde094584de9665d29a8
YashKinge <yashbkinge22@gmail.com>
2026-01-24 21:57:36 +0530

    added program

---
+++ workspace/src/com/java/math/AreaCircle.java
@@ -0,0 +1,12 @@
+package com.java.math;
+
+public class AreaCircle {
+
+	public static void main(String[] args) {
+		        double r = 5;
+		        double area = 3.14 * r * r;
+		        System.out.println("Area = " + area);
+
+	}
+
+}
