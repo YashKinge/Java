3ba9e0f48fb24f7ad71a72516a09c1fd022d6001
YashKinge <yashbkinge22@gmail.com>
2026-01-29 19:06:52 +0530

    added program

---
+++ workspace/src/com/java/math/GCD.java
@@ -0,0 +1,17 @@
+package com.java.math;
+
+public class GCD {
+	
+	public static void main(String[] args) {
+        int a = 24, b = 36;
+        int gcd = 1;
+
+        for(int i = 1; i <= a && i <= b; i++) {
+            if(a % i == 0 && b % i == 0)
+                gcd = i;
+        }
+
+        System.out.println("GCD = " + gcd);
+    }
+
+}
